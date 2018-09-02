ef8dd07dfbb9ccd493b55f7533493e84c4d35908
umarbabi <42767449+umarbabi@users.noreply.github.com>
2018-09-02 10:06:35 +0000

    Add files via upload

---
+++ A3_10667436.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+ 
+
+int main(void)
+
+{
+
+int mark;
+
+ 
+
+cout<<"Enter student's mark: ";
+
+cin>>mark;
+
+if (mark < 40)
+
+cout<<"Student grade = F";
+
+else if (mark < 50)
+
+cout<<"Student grade = E";
+
+else if (mark < 60)
+
+cout<<"Student grade = D";
+
+else if (mark < 70)
+
+cout<<"Student grade = C";
+
+else if (mark < 80)
+
+cout<<"Student grade = B";
+
+else
+
+cout<<"Student grade = A";
+
+cout<<"\n";
+
+ 
+
+return 0;
+
+}
